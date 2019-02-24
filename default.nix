c7ce05f2df7ef28aabf4ee7ac4c25188eea8e6b0
Guilherme <guilhermehas@hotmail.com>
2019-02-24 20:12:37 -0300

    changing to nix build

--- default.nix
+++ default.nix
@@ -1,5 +1,55 @@
-# nix-shell --pure --command 'cd src; make'
-let
-  nixpkgs = import <nixpkgs> {};
-in
-  nixpkgs.callPackage ./shell.nix {}
+{ pkgs ? import <nixpkgs> {} }: with pkgs;
+
+mkShell {
+  FONTCONFIG_FILE = makeFontsConf { fontDirectories = [ inconsolata-lgc libertine libertinus ]; };
+  buildInputs = [
+    (texlive.combine {
+        inherit (texlive)
+        fvextra
+        framed
+        newtx
+        nowidow
+        emptypage
+        wrapfig
+        subfigure
+        adjustbox
+        collectbox
+        tikz-cd
+        imakeidx
+        idxlayout
+        titlesec
+        subfiles
+        lettrine
+        upquote
+        libertine
+        mweights
+        fontaxes
+        mdframed
+        needspace
+        xifthen
+        ifnextok
+        currfile
+        noindentafter
+        ifmtarg
+        scheme-medium
+        listings
+        minted
+        microtype
+        babel
+        todonotes
+        chngcntr
+        ifplatform
+        xstring
+        minifp
+        titlecaps
+        enumitem;
+      })
+    gnumake
+    pythonPackages.pygments
+    which
+  ];
+  installPhase = ''
+    mkdir -p $out
+    cp out/* $out
+  '';
+}
--- shell.nix
+++ shell.nix
@@ -1,51 +1,5 @@
-{ pkgs ? import <nixpkgs> {} }: with pkgs;
-
-mkShell {
-  FONTCONFIG_FILE = makeFontsConf { fontDirectories = [ inconsolata-lgc libertine libertinus ]; };
-  buildInputs = [
-    (texlive.combine {
-        inherit (texlive)
-        fvextra
-        framed
-        newtx
-        nowidow
-        emptypage
-        wrapfig
-        subfigure
-        adjustbox
-        collectbox
-        tikz-cd
-        imakeidx
-        idxlayout
-        titlesec
-        subfiles
-        lettrine
-        upquote
-        libertine
-        mweights
-        fontaxes
-        mdframed
-        needspace
-        xifthen
-        ifnextok
-        currfile
-        noindentafter
-        ifmtarg
-        scheme-medium
-        listings
-        minted
-        microtype
-        babel
-        todonotes
-        chngcntr
-        ifplatform
-        xstring
-        minifp
-        titlecaps
-        enumitem;
-      })
-    gnumake
-    pythonPackages.pygments
-    which
-  ];
-}
+# nix-build shell.nix
+let
+  nixpkgs = import <nixpkgs> {};
+in
+  nixpkgs.callPackage ./shell.nix {}
